f55ea9a3d1d24964bccbaa9d9c7a3e753a5b02d8
Phil Hanna <ph1204@gmail.com>
2023-02-08 09:40:11 -0500

    Added unit tests for sorting

--- deck_test.go
+++ deck_test.go
@@ -5,6 +5,34 @@ import (
 	"testing"
 )
 
+func TestDeckSort(t *testing.T) {
+	deck := make(Deck, 0)
+	cards := []Card{
+		{TEN, CLUBS},
+		{JACK, DIAMONDS},
+		{TEN, SPADES},
+	}
+	for _, card := range cards {
+		deck = append(deck, card)
+	}
+	sort.Sort(&deck)
+	card0 := Card{TEN, CLUBS}
+	card1 := Card{TEN, SPADES}
+	card2 := Card{JACK, DIAMONDS}
+	if len(deck) != 3 {
+		t.Errorf("len(deck): want=%d,have=%d", 3, len(deck))
+	}
+	if deck[0] != card0 {
+		t.Errorf("card0: want=%v,have=%v", card0, deck[0])
+	}
+	if deck[1] != card1 {
+		t.Errorf("card1: want=%v,have=%v", card1, deck[1])
+	}
+	if deck[2] != card2 {
+		t.Errorf("card2: want=%v,have=%v", card2, deck[2])
+	}
+}
+
 func TestDeck(t *testing.T) {
 	deck := NewDeck()
 	printDeck("New deck", []Card(*deck))
--- pinochle_deck_test.go
+++ pinochle_deck_test.go
@@ -5,6 +5,35 @@ import (
 	"testing"
 )
 
+func TestPinochleDeckSort(t *testing.T) {
+	deck := make(PinochleDeck, 0)
+	cards := []Card{
+		{TEN, CLUBS},
+		{JACK, DIAMONDS},
+		{TEN, SPADES},
+	}
+	for _, card := range cards {
+		deck = append(deck, card)
+	}
+	sort.Sort(&deck)
+	card0 := Card{JACK, DIAMONDS}
+	card1 := Card{TEN, CLUBS}
+	card2 := Card{TEN, SPADES}
+	if len(deck) != 3 {
+		t.Errorf("len(deck): want=%d,have=%d", 3, len(deck))
+	}
+	if deck[0] != card0 {
+		t.Errorf("card0: want=%v,have=%v", card0, deck[0])
+	}
+	if deck[1] != card1 {
+		t.Errorf("card1: want=%v,have=%v", card1, deck[1])
+	}
+	if deck[2] != card2 {
+		t.Errorf("card2: want=%v,have=%v", card2, deck[2])
+	}
+
+	
+}
 func TestPinochleDeck(t *testing.T) {
 	deck := NewPinochleDeck()
 	printDeck("New pinochle deck", []Card(*deck))
