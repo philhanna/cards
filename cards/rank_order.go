18de7766a893d3d6752d7389157e2cac04435f7e
Phil Hanna <ph1204@gmail.com>
2023-02-07 23:36:40 -0500

    Added ordering for both regular and pinochle decks

--- cards/rank_order.go
+++ cards/rank_order.go
@@ -25,3 +25,28 @@ func (rs Ranks) Less(i int, j int) bool {
 func (rs Ranks) Swap(i int, j int) {
 	rs[i], rs[j] = rs[j], rs[i]
 }
+
+// PinochleRanks is a slice of Rank objects in a Pinochle deck
+type PinochleRanks []Rank
+
+// Less compares two Rank objects in a Pinochle deck and returns true
+// if the first one is less than the second one.
+func (prs PinochleRanks) Less(i int, j int) bool {
+	var rankI, rankJ int
+	rankI = pinochleRank(prs[i])
+	rankJ = pinochleRank(prs[j])
+	return rankI < rankJ
+}
+
+func pinochleRank(rank Rank) int {
+	switch rank {
+	case NINE, JACK, QUEEN, KING:
+		return int(rank)
+	case TEN:
+		return int(KING) + 1
+	case ACE:
+		return int(KING) + 2
+	default:
+		return -1
+	}
+}
